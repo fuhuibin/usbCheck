8ec2bf8c1fd132320c3f048036181212b1826aa4
Louis <Richard@freewindtekiMacBook-Pro.local>
2013-07-27 10:46:43 +0800

    Initial Commit

---
+++ usbCheck/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  usbCheck
+//
+//  Created by Louis on 13-7-27.
+//  Copyright (c) 2013年 Louis. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
